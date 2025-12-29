d59ff4454a652a1aab88fcfad2c34645e90f6675
rabihrajaa <rajaarabihh@gmail.com>
2025-12-29 10:29:32 +0100

    configuration de l archetecture des microservices

--- gateway-service/src/main/java/rabih/rajaa/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/rabih/rajaa/gatewayservice/GatewayServiceApplication.java
@@ -2,6 +2,9 @@ package rabih.rajaa.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
 
 @SpringBootApplication
 public class GatewayServiceApplication {
